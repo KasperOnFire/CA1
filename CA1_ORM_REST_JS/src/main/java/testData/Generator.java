03af28b71fcf0dfdb7981e4ffcde8761e09ec3fc
CaptainVillain <kristian.krog@gmail.com>
2017-09-12 11:16:12 +0200

    first arrays

--- CA1_ORM_REST_JS/src/main/java/testData/Generator.java
+++ CA1_ORM_REST_JS/src/main/java/testData/Generator.java
@@ -2,5 +2,12 @@ package testData;
 
 public class Generator
 {
+    String[] firstName = {Thomas, Peter, Anders, Mads, Michael, Signe, Lone, Hanne, Janne, Louise};
+    String[] lastName = {Andersen, Hansen, Thomsen, Madsen, Petersen, Jensen, Smith, Poulsen, Mchaelsen, Jansen};
+    String[] erhverv = {VVS, Tømrer, Smed, Elektriker, Murer, Maler};
+    String[] hobbies = {Tennis, Fodbold, Skydning, Skoleskyderi, Kunstmaler, Amagermanden-wannabe};
+    String[] streets = {Langgade, Kortgade, Gadegade, Skolegade, Kristengade, Jødegade, Muslimgade};
     
+    
+    }
 }
