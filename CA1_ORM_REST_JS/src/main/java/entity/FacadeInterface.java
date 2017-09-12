d5087ee2bcaec47b100ada92db2ee2dab561b020
SpankyHanky <kornholt@hotmail.com>
2017-09-12 11:14:23 +0200

    Added more methods to Interface

--- CA1_ORM_REST_JS/src/main/java/entity/FacadeInterface.java
+++ CA1_ORM_REST_JS/src/main/java/entity/FacadeInterface.java
@@ -14,9 +14,46 @@ public interface FacadeInterface {
     
     public Person getPerson(int phoneNumber);
     
-    public Company getCompany(int phoneNumber, int CVR);
+    public Company getCompany(int phone, int cvr);
     
-    public List<Person> getPersons(Hobby hobby);
+    public List<Person> getPersonsHobby(String hobby);
     
+    public List<Person> getPersonsCity(String city, int zipCode);
+    
+    public int getPersonsHobbyCount(String hobby);
+    
+    public int[] getZipCodes();
+    
+    public List<Company> getCompany(int employeeCount);
+    
+    public Person createPerson(Person person);
+    
+    public Person deletePerson(int id);
+    
+    public Person updatePerson(Person person);
+    
+    public Company createCompany(Company company);
+    
+    public Company deleteCompany(int cvr);
+    
+    public Company updateCompany(Company company);
+    
+    public Hobby createHobby(Hobby hobby);
+    
+    public Hobby deleteHobbby(String name);
+    
+    public Hobby updateHobby(Hobby hobby);
+    
+    public Phone createPhone(Phone phone);
+    
+    public Phone deletePhone(int number);
+    
+    public Phone updatePhone(Phone phone);
+    
+    public Address createAddress(Address address);
+    
+    public Address deleteAddress(String street, String additionalInfo);
+    
+    public Address updateAddress(Address address);
     
 }
