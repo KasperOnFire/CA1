9f20ae7c0d8de3eb0db2609d9e1ca14bad442350
kasperonfire <breindal70@gmail.com>
2017-09-15 13:29:06 +0200

    fixed js

--- CA1_ORM_REST_JS/src/main/webapp/JavaScript/companyScript.js
+++ CA1_ORM_REST_JS/src/main/webapp/JavaScript/companyScript.js
@@ -1,43 +1,38 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+function createCompany() {
+
+    var companyName = document.getElementById("CompanyName").value;
+    var companyDesc = document.getElementById("CompanyDescription").value;
+    var companyCVR = document.getElementById("CompanyCVR").value;
+    var companyNumEmp = document.getElementById("CompanyNumEmp").value;
+    var companyValue = document.getElementById("CompanyValue").value;
+
+    var apiLink = "http://localhost:8084/CA1_ORM_REST_JS/api/company";
+
+    var newCompany = {
+
+        name: companyName,
+        description: companyDesc,
+        cvr: companyCVR,
+        numEmployees: companyNumEmp,
+        marketValue: companyValue
+
+    };
+
+    var data = JSON.stringify(newCompany);
+    console.log(data);
+
+    fetch(apiLink, {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: data
+    }).then(function (res) {
+        return res.json();
+    }).then(function (data) {
+        alert("Company has been added to database!");
+    });
 
 
-
-
-function createCompany(){
-
-var companyName = document.getElementById("companyName").value;
-var companyDesc = document.getElementById("companyDescription").value;
-var companyCVR = document.getElementById("companyCVR").value;
-var companyNumEmp = document.getElementById("companyNumEmp").value;
-var companyValue = document.getElementById("companyValue").value;    
-    
-var apiLink = "http://localhost:8084/CA1_ORM_REST_JS/api/company";    
-
-var newCompany = {
-    
-    name: companyName,
-    description: companyDesc,
-    cvr: companyCVR,
-    numEmployees: companyNumEmp,
-    marketValue: companyValue
-    
-};
-
-var data = new FormData();
-data.append( "json", JSON.stringify( newCompany ) );
-
-fetch(apiLink,
-{
-    method: "POST",
-    body: data
-})
-.then(function(res){ return res.json(); })
-.then(function(data){ alert( JSON.stringify( data ) ) });
-
-   
 }
-
