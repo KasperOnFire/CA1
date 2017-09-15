60572de2ce70c084fde863842b72398110bda803
SpankyHanky <kornholt@hotmail.com>
2017-09-15 13:41:03 +0200

    added person JS

--- CA1_ORM_REST_JS/src/main/webapp/JavaScript/personScript.js
+++ CA1_ORM_REST_JS/src/main/webapp/JavaScript/personScript.js
@@ -1,7 +1,35 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+function createPerson() {
+
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    
+
+    var apiLink = "http://localhost:8084/CA1_ORM_REST_JS/api/person";
+
+    var newPerson = {
+
+        firstName: companyName,
+        lastName: companyDesc,
+
+    };
+
+    var data = JSON.stringify(newPerson);
+    console.log(data);
+
+    fetch(apiLink, {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: data
+    }).then(function (res) {
+        return res.json();
+    }).then(function (data) {
+        alert("Person has been added to database!");
+    });
+
+
+}
 
 
