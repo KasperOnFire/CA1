81f62a722ad24a8d96ae3a2a3cccd170c4b664e0
Unknown <cph-kj209@cphbusiness.dk>
2017-09-15 14:20:44 +0200

    Auto stash before merge of "master" and "origin/master"
    Added first test (REST assured)

---
+++ CA1_ORM_REST_JS/src/test/java/rest/TestPersonResources.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import org.junit.Test;
+
+/**
+ *
+ * @author KnaldeKalle
+ */
+public class TestPersonResources {
+    
+    
+    @Test
+    public void getJson(){
+    given().when().get("http://dev.breindal.me/CA1_ORM_REST_JS/api/person/complete/").then().statusCode(200);
+    given().when().get("http://dev.breindal.me/CA1_ORM_REST_JS/api/person/complete/").then().body(containsString("Kim")); 
+}
+    
+    
+    
+    
+    
+}
