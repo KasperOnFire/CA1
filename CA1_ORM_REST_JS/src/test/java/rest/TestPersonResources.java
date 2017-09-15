42083b874ae65f0470f344344733429044de40d4
Unknown <cph-kj209@cphbusiness.dk>
2017-09-15 14:31:39 +0200

    Mooooorrre tesssstts!

--- CA1_ORM_REST_JS/src/test/java/rest/TestPersonResources.java
+++ CA1_ORM_REST_JS/src/test/java/rest/TestPersonResources.java
@@ -19,12 +19,22 @@ public class TestPersonResources {
     
     @Test
     public void getJson(){
-    given().when().get("http://dev.breindal.me/CA1_ORM_REST_JS/api/person/complete/").then().statusCode(200);
-    given().when().get("http://dev.breindal.me/CA1_ORM_REST_JS/api/person/complete/").then().body(containsString("Kim")); 
+        given().when().get("http://dev.breindal.me/CA1_ORM_REST_JS/api/person/complete/").then().statusCode(200);
+        given().when().get("http://dev.breindal.me/CA1_ORM_REST_JS/api/person/complete/").then().body(containsString("Kim"));
 }
     
+    @Test
+    public void getByIdJson(){      
+        given().when().get("http://dev.breindal.me/CA1_ORM_REST_JS/api/person/complete/55").then().body("firstName",equalTo("Rusty"));       
+    }
     
+    @Test
+    public void getContactAll(){
+        given().when().get("http://dev.breindal.me/CA1_ORM_REST_JS/api/person/contactinfo").then().body(containsString("Lenormand"));
+    }
     
-    
-    
+    @Test
+    public void getContactFromId(){
+        given().when().get("http://dev.breindal.me/CA1_ORM_REST_JS/api/person/contactinfo/52").then().body(containsString("Lenormand"));
+    }
 }
